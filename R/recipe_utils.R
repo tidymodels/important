eea445497d8d9d473c9ea6a32d0527712fb47021
topepo <mxkuhn@gmail.com>
2025-08-20 10:54:51 -0400

    no longer needed

--- R/recipe_utils.R
+++ R/recipe_utils.R
@@ -1,9 +1,3 @@
-# Check input for step_predictor_desirability()
-check_desirability_arg <- function(x) {
-	# check for `c()`
-	# check not empty
-}
-
 # For an expression or quosure, list the user-specificied score functions
 extract_score_names <- function(x, call = rlang::caller_env()) {
 	if (rlang::is_quosure(x)) {
